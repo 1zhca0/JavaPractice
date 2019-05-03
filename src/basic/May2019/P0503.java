261bbf0147ebc64c7896dc5302c54e63ca391f54
YiZhengCao <YiZhengCao@9.112.164.118>
2019-05-03 15:59:26 +0800

    checked May 03 changes

---
+++ src/basic/May2019/P0503.java
@@ -0,0 +1,13 @@
+package basic.May2019;
+
+public class P0503 {
+	
+	/*
+	 * 	类中有一个方法，方法名makePi;
+		返回一个包含pi(参考Math.PI)的前n位数字的整数数组长度，n为方法接收的参数。
+		例如：n为3，则返回{3，1，4}。
+	 */
+	public void getNumbers() {
+		
+	}
+}
