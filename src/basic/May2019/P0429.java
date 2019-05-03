9995fa8f26fe39e6eec0a7aff3f13ef2aa04efe3
YiZhengCao <YiZhengCao@9.112.164.118>
2019-05-03 15:27:46 +0800

    优化算法

--- src/basic/May2019/P0429.java
+++ src/basic/May2019/P0429.java
@@ -1,40 +1,40 @@
 package basic.May2019;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class P0429 {
 
 	/*
 	 * 给定一个数组, 获取其中所有首字母, 并给出这个数组中以这个首字母为开头的所有String对象的拼接
-	 */
+	 */	
+	public static Map<String,String> firstChar(String[] arr){
+        
+        Map<String,String> map = new HashMap<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            //获取首字符
+            char c = arr[i].charAt(0);
+            String s = "" + c;
+            //如果首字符集合中没有，直接添加,如果有，拼接字符串后添加
+            if(!map.containsKey(s)){
+                map.put(s,arr[i]);
+            } else {
+                String newStr = map.get(s) + arr[i];
+                map.put(s, newStr);
+            }
+        }
+//        return toString(map);
+        
+        return map;
+    }
+	
 	public static void main(String[] args) {
 		
-		String sample[] = {"salt","tea","soda","toast","hello"};
-		
-		Set<String> firstsSet = new HashSet<>();
-		for (int i = 0; i < sample.length; i++) {
-			firstsSet.add(sample[i].substring(0, 1));
-		}
-		
-		
-		System.out.println(firstsSet);
-		
-		String result = "";
-		
-		for (String str : firstsSet) {
-			String temp = "";
-			for (int j = 0; j < sample.length; j++) {
-				
-				if (sample[j].substring(0, 1).equals(str)) {
-					temp += sample[j];
-				}
-			}
-			
-			temp = str + ": " + temp + ". ";
-			result = result + " " + temp;
-		}
-		
-		System.out.println(result);
+		String s[] = {"salt","tea","soda","toast","hello", "ammm", "b123"};
+		new P0429();
+		System.out.println(P0429.firstChar(s).values());
 	}
+	
+	
 }
