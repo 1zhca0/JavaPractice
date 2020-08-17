cfb6d50d2b630568bfada8793076d8e7be3bb7c3
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-08-17 21:08:23 +0800

    Thread knowledge

---
+++ src/main/java/basic/Aug2020/threads/L5Daemon.java
@@ -0,0 +1,34 @@
+package basic.Aug2020.threads;
+
+public class L5Daemon implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        Thread daemonThread = new Thread(new MyDaemon());
+        Thread normalThread = new Thread(()->{
+            for (int i = 0; i < 50; i++) {
+                System.out.println("Normal Thread " + i);
+            }
+            System.out.println("==Normal Stopped==");
+        });
+        daemonThread.setDaemon(true);
+
+        normalThread.start();
+        daemonThread.start();
+    }
+}
+
+class MyDaemon implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println("Daemon Thread " + i);
+        }
+        System.out.println("==Daemon Stoped==");
+    }
+}
