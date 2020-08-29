db7e9f92bbc8d8f759ae565a4a8ad2814d94e055
cyz <yizheng1991@hotmail.com>
2020-08-29 10:45:49 +0800

    commit

---
+++ src/main/java/basic/Aug2020/threads/L6UnsafeThreads.java
@@ -0,0 +1,31 @@
+package basic.Aug2020.threads;
+
+public class L6UnsafeThreads {
+    public static void main(String[] args) {
+
+       BuyTicket b = new BuyTicket();
+        new Thread(b).start();
+        new Thread(b).start();
+        new Thread(b).start();
+    }
+}
+
+class BuyTicket implements Runnable {
+
+    private  int Tickets = 10;
+    private  boolean flag = true;
+    @Override
+    public void run() {
+        while (flag)
+            buy();
+    }
+
+    void buy(){
+
+        if (Tickets<=0) {
+            System.out.println("no ticket left");
+            flag=false;
+        }
+        System.out.println(Thread.currentThread().getName() + " get No." + Tickets-- );
+    }
+}
---
+++ src/main/java/basic/Aug2020/threads/L6Unsafebank.java
@@ -0,0 +1,79 @@
+package basic.Aug2020.threads;
+
+import com.sun.tools.doclint.Entity;
+import lombok.SneakyThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sun.tools.doclint.Entity.or;
+
+
+public class L6Unsafebank {
+
+    public static void main(String[] args) throws InterruptedException {
+//        Account life = new Account(100, "life");
+//        GetMoney frank = new GetMoney(life, 20);
+//        GetMoney wife = new GetMoney(life, 100);
+//
+//        new Thread(wife, "wife").start();
+//        new Thread(frank, "frank").start();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            int finalI = i;
+            new Thread(()->{
+               list.add(finalI);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+        }
+//        Thread.sleep(30000);
+        System.out.println("list.size() - " + list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("the " + i + " -  " + list.get(i));
+        }
+        System.out.println("list.size() - " + list.size());
+    }
+}
+
+class Account{
+    int money;
+    String name;
+
+    Account(int money, String name) {
+        this.money = money;
+        this.name = name;
+    }
+}
+
+class GetMoney implements Runnable{
+
+    private Account account;
+    private int getMoney;
+
+    GetMoney(Account account, int getMoney){
+        this.account = account;
+        this.getMoney = getMoney;
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        if (getMoney>account.money) {
+            System.out.println("no enough money");
+            return;
+        }
+        Thread.sleep(1000);
+        System.out.println(Thread.currentThread().getName() + " get : " + getMoney);
+        account.money = account.money - getMoney;
+        System.out.println(account.name + " Left money : " + account.money);
+    }
+
+}
