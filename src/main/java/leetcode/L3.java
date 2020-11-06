b3a37990dc4190c53561483bfdc8247e8769b20d
1zca0 <yizheng1991@hotmail.com>
2020-11-06 10:43:33 +0800

    commit lession 3

--- src/main/java/leetcode/L3.java
+++ src/main/java/leetcode/L3.java
@@ -4,8 +4,16 @@ package leetcode;
 public class L3 {
     public static void main(String[] args) {
         String s = "abcabd";
+        int result = 0;
         for (int i = 0; i < s.length(); i++) {
-
+        	System.out.println("---");
+        	char flag1 = s.charAt(i);
+    		System.out.println("第" + i + " 次循环， --> " + flag1);
+    		
+    		for (int j = i; j < s.length(); j++) {
+				
+			}
+    		
         }
     }
 }
