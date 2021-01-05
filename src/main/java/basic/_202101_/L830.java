5d245e28b54da72cacf00cd45a2a674505594870
1zca0 <yizheng1991@hotmail.com>
2021-01-05 16:32:34 +0800

    commit leetcode

---
+++ src/main/java/basic/_202101_/L830.java
@@ -0,0 +1,26 @@
+package basic._202101_;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ProjectName: JavaPractice
+ * @Package: basic._202101_
+ * @ClassName: L830
+ * @Author: Frank.Cao
+ * @Description: LeetCodeLession830
+ * @Date: 2021/1/5 10:30
+ * @Version: 1.0
+ */
+public class L830 {
+    public static void largeGroupPositions(String s) {
+
+
+    }
+
+    public static void main(String[] args) {
+        largeGroupPositions("abcccd");
+    }
+}
